9e1d88f65e5258840ac72f4557234b7ed8037640
Christian Carsin <contact@muldev.dev>
2020-02-22 21:28:07 +0100

    fixpy

--- modelos/Cliente.py
+++ modelos/Cliente.py
@@ -6,3 +6,10 @@ class Cliente(models.Model):
 	telefono = fields.Integer('Telefono', required=True)
 	direccion = fields.Char('Direccion', required=True)
 	nif = fields.Char('NIF', required=True)
+
+	def name_get(self):
+		res=[]
+		for record in self:
+			name = record.nombre
+			res.append((record.id, name))
+		return res
