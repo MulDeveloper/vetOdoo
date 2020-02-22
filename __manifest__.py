23e75d45fe1f66207bfe2cc482acf8c78041a5f5
Christian Carsin <contact@muldev.dev>
2020-02-22 21:37:38 +0100

    fixpy

--- __manifest__.py
+++ __manifest__.py
@@ -5,5 +5,5 @@
     'version': '1.0',
     'website': 'muldev.dev',
     'depends': ['base'],
-    'data': ['views/cliente.xml', 'views/mascota.xml', "views/consulta.xml"]
+    'data': ['views/cliente.xml', 'views/mascota.xml', "views/consulta.xml", "reports/report.xml", "reports/consulta_card.xml"]
 }
